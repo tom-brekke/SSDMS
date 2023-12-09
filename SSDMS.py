cdc03839b37b1fc07ff7d57118087c30d319aef6
Tom Brekke <tom.brekke@gmail.com>
2023-12-09 21:07:04 +0000

    Update SSDMS.py
    includes the twitter bits now

--- SSDMS.py
+++ SSDMS.py
@@ -11,6 +11,9 @@ import pyaudio
 import numpy as np
 import datetime
 import re
+import subprocess
+import twitter
+import random
 
 #this script should be executed when you want to begin monitoring for dog barks
 
@@ -25,6 +28,7 @@ parser.add_argument("--attachment", "-a", type=str, required=False, default="", 
 parser.add_argument("--time", type=int, required=False, help="time in minutes to monitor the sausage dog, default: 1435 (24 hours = 1440 minutes)", default=1435)
 parser.add_argument("--data", type=str, required=False, help="the file to write the data to. Filename will be preceded by the current date if the time is set to default of 1435 minutes (i.e. just shy of 24 hours which would be 1440)")
 parser.add_argument("--threshhold", type=int, required=False, help="the threshold that will trigger an email - 2500 is standard quiet house, >15000 is loud barking, default: 15000", default=15000)
+parser.add_argument("--No_tweet", default=None, action='store_const', const=True, required=False, help="Suppress tweets from Charly and Madison's account")
 
 
 args=parser.parse_args()
@@ -35,6 +39,8 @@ attachment=args.attachment
 time=args.time
 threshhold=args.threshhold
 outfile=args.data
+toTweet = not args.No_tweet
+
 if outfile:
 	if time==1435:	#1400 is default
 		today = datetime.datetime.now().strftime("%Y-%m-%d")
@@ -69,6 +75,14 @@ def SendEmail(sendto, subject, body, attachment, user, passwd):
 	s.sendmail(msg['From'], msg['To'], msg.as_string())
 	s.quit()
 
+#here's the twitter stuff:
+if toTweet:
+    AccessToken="979743511007424512-vvznXVrN9KKCcaEoFbNzds0rVHbbheX"
+    AccessTokenSecret="fKyiSLHfy9RdrNVLnAMaGmDa2EQQGG1sl439ySwgGfryZ"
+    APIKey="CBP4XNuEGkOsPWPuVgjBLVy6s"
+    APISecret="NHKeueHH5UPgKFAbU0r6Qv7z2GRl1Az1xu3l1gTL6wMLkAaDpd"
+    api=twitter.Api(consumer_key=APIKey, consumer_secret=APISecret, access_token_key=AccessToken, access_token_secret=AccessTokenSecret)
+    BarkList=["BARK", "bark", "Bark", "BARK!", "barkbarkbark", "BARK BARK BARK BARK BARK", "#bark", "#BarkBarkBark", "woof"]     
 
 CHUNK = 2**11
 RATE = 8000
@@ -87,6 +101,26 @@ for i in range(int(time*(235))): #235 came from finding that there are 235 sampl
 	if outfile:
 		now = datetime.datetime.now()
 		print(peak, now.strftime("%Y-%m-%d %H:%M"), file=OUT)
+	if toTweet:
+            #oldStatus=api.GetUserTimeline(screen_name="@Dale_n_Dachs", count=1)[0].text
+            #print(j, oldStatus, file=sys.stderr)
+            j=random.randint(0, (len(BarkList)-1))
+            print("\n 1. Trying tweet with j=", j,"and tweet is: ", BarkList[j], file=sys.stderr)
+            try:
+                api.PostUpdate(BarkList[j])
+            except:
+                print("\n 2. Didn't seem to work. Trying with j=1 if j was 0 or j=j-1 otherwise", file=sys.stderr)
+                if j==0:
+                    k=1
+                else:
+                    k=j-1
+                try:
+                    api.PostUpdate(BarkList[k])
+                except:
+                    print("\n\n\nfor some reason, the tweets are throwing errors.", file=sys.stderr, end="\n\n\n")
+    if now.strftime("%H:%M") == "23:59":   
+        break
+
 stream.stop_stream()
 stream.close()
 p.terminate()
